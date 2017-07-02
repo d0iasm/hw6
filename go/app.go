b62451f1c4a59b3dbfc57ccec7cc1d29ae26ffa9
Asami Doi <doiasami1219@gmail.com>
2017-07-02 20:07:04 +0900

    update use stylesheet

--- go/app.go
+++ go/app.go
@@ -2,17 +2,19 @@ package app
 
 import (
 	// "fmt"
-	"net/http"
 	"html/template"
+	"net/http"
 )
 
 func init() {
+	http.Handle("./stylesheets/", http.StripPrefix("./stylesheets/", http.FileServer(http.Dir("./stylesheets/"))))
 	http.HandleFunc("/", handlePata)
 }
 
 func handlePata(w http.ResponseWriter, r *http.Request) {
 	// w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	// fmt.Fprintf(w, "Hello world!!!!\n")
+	result := "hoge"
 	tmpl := template.Must(template.ParseFiles("./index.html"))
-	tmpl.Execute(w, "hoge")
+	tmpl.Execute(w, result)
 }
